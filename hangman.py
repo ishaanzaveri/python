4b57a084b494cac3aec1e23ac58252ab57c6fa3d
Ishaan <ishaan@IshaanZaveri.local>
2018-05-08 11:27:41 +0530

    finished hangman

--- hangman.py
+++ hangman.py
@@ -1,28 +1,28 @@
 #HANGMAN
-
+from subprocess import call
 HangManList=["H","A","N","G","M","A","N"]
 wordList = []
 num=0
 n=0
 Word = input("Enter word \n")
+call("clear")
 for i in range (0,len(Word)):
 	wordList.append("X")
 
 done = False
 
+
 while done != True:
 	print (wordList)
 	char = input("Enter character ")
 	if char in Word :
-		charNum = Word.find(char)
-		print(charNum)
-		wordList[charNum] = Word[charNum]
-		print(wordList)
+		for i in range(0,len(Word)):
+			if Word[i]==char:
+				wordList[i] = Word[i]
 		if wordList.count("X")<1:
+			print(wordList)
 			print("you won!!")
 			break
-		Word=Word.replace(char," ",Word.count(char))
-		print(Word)
 		print(wordList)
 	else :
 		print(HangManList[0:num])
